d7cfc75852d2241357a14ae0c14b2e4615831be5
David Park <68914460+hydrol88@users.noreply.github.com>
2020-08-09 22:24:00 +0900

    Add files via upload

---
+++ simple_perceptropn1.py
@@ -0,0 +1,16 @@
+# 퍼셉트론 
+# AND(논리곱) 게이트
+w1 = 0.5
+w2 = 0.5
+b = 0.0
+
+def perceptron(x1, x2):
+    y = w1 * x1 + w2 * x2 + b   
+    if y > 0.5:
+        return 1
+    else:
+        return 0
+    
+for x1, x2 in [(0,0), (0,1), (1,0), (1,1)]:
+    print("입력: ", x1, x2, " 출력", perceptron(x1, x2))
+
